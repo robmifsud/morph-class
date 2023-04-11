2c06e58344f82b972f39708fd1168260052b14ee
Robert Mifsud <robert.mifs@gmail.com>
2023-04-11 19:29:06 +0200

    Updated parameters script
    FIxed bug in the regex class prefixes. Many additional options to set before running scripts

--- Scripts/parameters.py
+++ Scripts/parameters.py
@@ -5,19 +5,43 @@
 import os
 from datetime import datetime
 from tensorflow.keras import metrics
+from tensorflow.keras.optimizers import RMSprop, Adam
 from tensorflow_addons.metrics import F1Score
+# import tensorflow as tf
+# tf.keras.optimizers.Adam()
 
 # Runtime variables !IMPORTANT!
 
-MODEL = 'Inception_v1'
+# MODEL = 'Inception_v1'
 # MODEL = 'LeNet'
+MODEL = 'Sanchez'
+# MODEL = 'AlexNet'
+
+EPOCHS = 250
+BATCH_SIZE = 32
+NORMALISE = True
+AUGMENT = True
+# OPTIMIZER = Adam(learning_rate=0.001)
+OPTIMIZER = Adam()
+# OPTIMIZER = RMSprop()
+# load last saved model, including weights
+LOAD_MODEL = False
+
+# if not loading model, then you can load last saved weights
+# LOAD_WEIGHTS = False
+# SAVE_MODEL = True
+
+# Dataset variables
+# UNBATCHED_SIZE = 239029 # Full dataset
+# UNBATCHED_SIZE = 20
+UNBATCHED_SIZE = 120432 # classification reduced with a(p) >= 0.3 & v >= 5
+# NUM_CLASSES = 2
+NUM_CLASSES = 3
 RESIZE = True
 OG_IMAGE_SIZE = 424
-IMAGE_SIZE = 224
-BATCH_SIZE = 32
-LOAD_MODEL = False
-LOAD_WEIGHTS = True
-NUM_CLASSES = 3
+IMAGE_SIZE = 69
+# IMAGE_SIZE = 224
+# IMAGE_SIZE = 227
 
 # Current path
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -49,35 +73,38 @@ path_gz2_merge = os.path.join(dir_temp, "class_with_map.csv")
 gz2_3_class         = ['E', 'S', 'SB']
 gz2_3_class_regex   = {
                         'E' : '^E.*$', 
-                        'S' : '^S[^B]+$', 
+                        # 'S' : '^S[^B]+$', bug here as includes Se(edge-on) galaxies?
+                        'S' : '^S[^Be]+$',
                         'SB': '^SB.*$'
 }
 
-gz2_7_class     = ['E', 'Sa', 'Sb', 'Sc','Se', 'SBa', 'SBc']
+gz2_7_class     = ['E', 'Sa', 'Sb', 'Sc', 'SBa', 'SBb', 'SBc']
 gz2_7_class_regex = {
                         'E'  : '^E.*$', 
                         'Sa' : '^Sa.*$',
                         'Sb' : '^Sb.*$',
                         'Sc' : '^Sc.*$',
-                        'Se' : '^Se.*$',
+                        # 'Se' : '^Se.*$',
                         'SBa': '^SBa.*$',
+                        'SBb': '^SBb.*$',
                         'SBc': '^SBc.*$'
 }
 
-gz2_9_class     = ['E', 'Sa', 'Sb', 'Sc','Sd', 'Se', 'SBa', 'SBc', 'SBd']
+gz2_9_class     = ['E', 'Sa', 'Sb', 'Sc','Sd', 'SBa', 'SBb', 'SBc', 'SBd']
 gz2_9_class_regex = {
                         'E'  : '^E.*$', 
                         'Sa' : '^Sa.*$',
                         'Sb' : '^Sb.*$',
                         'Sc' : '^Sc.*$',
                         'Sd' : '^Sd.*$',
-                        'Se' : '^Se.*$',
+                        # 'Se' : '^Se.*$',
                         'SBa': '^SBa.*$',
+                        'SBb': '^SBb.*$',
                         'SBc': '^SBc.*$',
                         'SBd': '^SBd.*$'
 }
 
-gz2_11_class    = ['Er','Ei','Ec', 'Sa', 'Sb', 'Sc','Sd', 'Se', 'SBa', 'SBc', 'SBd']
+gz2_11_class    = ['Er','Ei','Ec', 'Sa', 'Sb', 'Sc','Sd', 'SBa', 'SBb', 'SBc', 'SBd']
 gz2_11_class_regex = {
                         'Er'  : '^Er.*$',  
                         'Ei'  : '^Ei.*$', 
@@ -86,14 +113,16 @@ gz2_11_class_regex = {
                         'Sb' : '^Sb.*$',
                         'Sc' : '^Sc.*$',
                         'Sd' : '^Sd.*$',
-                        'Se' : '^Se.*$',
+                        # 'Se' : '^Se.*$',
                         'SBa': '^SBa.*$',
+                        'SBb': '^SBb.*$',
                         'SBc': '^SBc.*$',
                         'SBd': '^SBd.*$'
 }
 
 # Training metrics
-metrics = [metrics.Accuracy(), metrics.Precision(), metrics.Recall(), metrics.MeanSquaredError(), metrics.MeanAbsoluteError(), F1Score(NUM_CLASSES)]
+# metrics = [metrics.Accuracy(), metrics.Precision(), metrics.Recall(), metrics.MeanSquaredError(), metrics.MeanAbsoluteError(), F1Score(NUM_CLASSES)]
+metrics = ['accuracy', metrics.Precision(), metrics.Recall(), metrics.MeanSquaredError(), metrics.MeanAbsoluteError()]
 
 ## Path to models
 dir_models = os.path.join(dir_data, 'Models')
@@ -106,4 +135,4 @@ dir_terminal = os.path.join(dir_data, 'Terminal')
 
 # Path to terminal output file
 dt = datetime.now()
-path_terminal_output = os.path.join(dir_terminal, f'{MODEL}_{dt.date()}_{dt.hour};{dt.minute}.csv')
+path_terminal_output = os.path.join(dir_terminal, f'{MODEL}_{NUM_CLASSES}class_{dt.date()}_{dt.hour};{dt.minute}.csv')

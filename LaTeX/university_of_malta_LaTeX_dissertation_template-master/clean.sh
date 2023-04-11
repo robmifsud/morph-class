afbe81e44d8dc7ed3e7a6c3e8cf338de6f712fab
Robert Mifsud <robert.mifs@gmail.com>
2023-04-11 21:24:19 +0200

    Moved report

--- LaTeX/university_of_malta_LaTeX_dissertation_template-master/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# that .git exclusion is super important ...
-find . -regex ".*[^\.git].*\(aux\|bbl\|blg\|idx\|log\|lof\|lot\|nav\|out\|snm\|toc\|synctex.gz\)$" -not -path '*/.git/*' -exec rm -vf {} \;
